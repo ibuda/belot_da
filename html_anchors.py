e41fda54e9f31d197ab793b8bb953327c5759e19
ibuda <ivan.budanaev@gmail.com>
2020-01-30 19:01:31 +0200

    Added requirement.txt and html_anchors files

---
+++ html_anchors.py
@@ -0,0 +1,7 @@
+"""
+File with various html tags, or keywords, or any other
+text that is related to useful information needed to
+be parsed. 
+"""
+
+PLAYER_NAME_STYLE = 'border-bottom:1px solid;color:lightgray'
