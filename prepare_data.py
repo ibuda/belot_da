dc1d068a8cc11b417fc88565f24a056eca7a9917
ibuda <ivan.budanaev@gmail.com>
2020-02-09 13:39:29 +0200

    added winner text parsing, and winners player names

--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,13 @@
 from datetime import datetime
 
+game_categories = {
+    'Jucatorul ': 0,
+    'Jocul a fost castigat de': 1,
+    'Razboi intre clanuri, ': 2,
+    'Campionat etapa: ': 3,
+    'Liga ': 4,
+}
+
 
 def parse_game_date(text: str) -> datetime:
     # parsing custom data string to datetime
@@ -15,8 +23,28 @@ def parse_game_date(text: str) -> datetime:
 
 def parse_game_duration(text: str) -> int:
     # returns game duration in seconds
+    if text is None or text == '':
+        return 0
     res = int(text[:2]) * 3600
     res += int(text[3:5]) * 60
     res += int(text[6:8])
 
     return res
+
+
+def parse_winner_text(text: str):
+    for key, val in game_categories.items():
+        if text.startswith(key):
+            return val
+
+    return -1
+
+
+def parse_winners(text: str, game_type: int):
+    # getting winner player name
+    if game_type == 0:
+        text = text[10:]
+        return text[: text.find(' ')]
+    elif game_type == 1:
+        text = text[25:]
+        return text[: text.find(' ')]
